0ece5f87eec4108d1059744fbb86efde078b8871
asadhky <77893429+asadhky@users.noreply.github.com>
2023-08-11 11:52:11 +0500

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,38 +1,51 @@
-"use client"
+import { Box, Button, Stack } from "@mui/material";
+import { ContextValueReady } from "eth.context";
+import { JobOffer } from "models";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import styles from './index.module.css';
 
-import { NotReadyReason, useEth } from "@/eth.context";
-import styles from "./page.module.css";
-
-export default function Home(): JSX.Element {
-  const eth = useEth();
-  if (!eth.ready) {
-    switch (eth.notReadyReason) {
-      case NotReadyReason.Initializing:
-        return <p>Initializing...</p>;
-      case NotReadyReason.NoWallet:
-        return <p>⚠️ Cannot find wallet.</p>;
-      case NotReadyReason.NoArtifact:
-        return (
-          <p>
-            ⚠️ Cannot find <span className="code">SolJobs</span> contract
-            artifact. Please complete the above preparation first.
-          </p>
-        );
-      case NotReadyReason.NoAccount:
-        return <p>⚠️ Wallet not connected.</p>;
-      case NotReadyReason.WrongNetwork:
-        return (
-          <p>
-            ⚠️ MetaMask is not connected to the same network as the one you
-            deployed to.
-          </p>
-        );
-    }
-  }
+export default function Home({ eth }: { eth: ContextValueReady }): JSX.Element {
+  const [jobs, setJobs] = useState<JobOffer[]>([]);
+  const [loading, setLoading] = useState(false);
   
+  useEffect(() => {
+    const getJobs = async () => {
+      if (!eth.ready) return;
+      setLoading(true);
+
+      const fetchedJobs: JobOffer[] = await eth.contracts.solJobs.methods.getAllJobsCreated.call().call();
+      setJobs(Array.from(fetchedJobs).sort((a, b) => b.id - a.id)); // sort from newest to oldest
+      
+      setLoading(false);
+    }
+
+    getJobs();
+
+  }, [eth])
+
+  if (loading) {
+    return <p>Fetching jobs...</p>
+  }
+
+  if (!loading && jobs.length === 0) {
+    return <p>No jobs found.</p>
+  }
+
   return (
-    <div className={styles.container}>
-      <p>Smart contract successfully deployed to <code>{eth.contracts.solJobs.options.address}</code></p>
+    <div>
+      {jobs.map((job: JobOffer) => {
+        return (
+          <Stack className={styles.container} key={job.id}>
+            <Box>
+              <h3>{job.title}</h3>
+              <span className={styles.metadata}><Link href={`/profile/c/${job.creator.creatorAddress}`}>{job.creator.name}</Link></span>
+              <span className={styles.metadata}>{eth.web3.utils.fromWei(job.compensation.toString(), 'ether')} ETH</span>
+            </Box>
+            <Button variant="contained" size='small'>Apply</Button>
+          </Stack>
+        )
+      })}
     </div>
   );
 }
